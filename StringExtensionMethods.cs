90bb17bbe36f40ebc3127d2a4bd868ee2c65661a
Georg <georg@georgdangl.de>
2015-09-26 15:57:52 +0200

    Added string extension methods

---
+++ StringExtensionMethods.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Dangl
+{
+    public static class StringExtensionMethods
+    {
+        /// <summary>
+        /// Will replace all linebreaks with <see cref="Environment.NewLine"/> and remove white spaces as line ends as well as any trailing white spaces.
+        /// </summary>
+        /// <param name="Input"></param>
+        /// <returns></returns>
+        public static string Sanitize(this string Input)
+        {
+            var Splitted = Regex.Split(Input, "\r\n?|\n");
+            var ReturnString = string.Empty;
+            for (int i = 0; i < Splitted.Length; i++)
+            {
+                ReturnString += Splitted[i].TrimEnd() + Environment.NewLine;
+            }
+            return ReturnString.TrimEnd();
+        }
+    }
+}
