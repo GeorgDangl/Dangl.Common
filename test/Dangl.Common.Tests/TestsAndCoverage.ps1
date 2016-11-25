409aba35f9c6400379a0307e3bdb0a20751dc73d
Georg Dangl <georg@georgdangl.de>
2016-11-25 13:58:01 +0100

    Update test script

--- test/Dangl.Common.Tests/TestsAndCoverage.ps1
+++ test/Dangl.Common.Tests/TestsAndCoverage.ps1
@@ -1,9 +1,9 @@
 # Get the most recent OpenCover NuGet package from the dotnet nuget packages
 $nugetOpenCoverPackage = Join-Path -Path $env:USERPROFILE -ChildPath "\.nuget\packages\OpenCover"
 $latestOpenCover = Join-Path -Path ((Get-ChildItem -Path $nugetOpenCoverPackage | Sort-Object Fullname -Descending)[0].FullName) -ChildPath "tools\OpenCover.Console.exe"
-# Get the most recent ReportGenerator NuGet package from the dotnet nuget packages
-$nugetReportGeneratorPackage = Join-Path -Path $env:USERPROFILE -ChildPath "\.nuget\packages\ReportGenerator"
-$latestReportGenerator = Join-Path -Path ((Get-ChildItem -Path $nugetReportGeneratorPackage | Sort-Object Fullname -Descending)[0].FullName) -ChildPath "tools\ReportGenerator.exe"
+# Get the most recent OpenCoverToCoberturaConverter from the dotnet nuget packages
+$nugetCoberturaConverterPackage = Join-Path -Path $env:USERPROFILE -ChildPath "\.nuget\packages\OpenCoverToCoberturaConverter"
+$latestCoberturaConverter = Join-Path -Path (Get-ChildItem -Path $nugetCoberturaConverterPackage | Sort-Object Fullname -Descending)[0].FullName -ChildPath "tools\OpenCoverToCoberturaConverter.exe"
 # Arguments for running dotnet
 $dotnetArguments = "test", "`"`"$PSScriptRoot`"`"", "-xml `"`"$PSScriptRoot\xUnit.testresults`"`""
 "Running tests with OpenCover"
@@ -15,4 +15,8 @@ $dotnetArguments = "test", "`"`"$PSScriptRoot`"`"", "-xml `"`"$PSScriptRoot\xUni
 -output:"$PSScriptRoot\OpenCover.coverageresults" `
 -excludebyattribute:System.CodeDom.Compiler.GeneratedCodeAttribute `
 "-filter:+[Dangl*]* -[*.Tests]* -[*.Tests.*]*"
-"Creating Code Coverage Html report"& $latestReportGenerator `"-reports:$PSScriptRoot\OpenCover.coverageresults" `"-targetdir:$PSScriptRoot\CoverageReport" `"-historydir:C:\CoverageHistories\Dangl.Common"
+ "Converting coverage reports to Cobertura format"
+& $latestCoberturaConverter `
+-input:"$PSScriptRoot\OpenCover.coverageresults" `
+-output:"$PSScriptRoot\Cobertura.coverageresults" `
+-sources:"$PSScriptRoot"
