6fb69ad06b5b2b7f94c01a8cbafb6e92d1da8162
Georg Dangl <georg@georgdangl.de>
2016-11-25 21:59:39 +0100

    Rework StringEcnryption to work with any passwords

--- src/Dangl.Common/StringEncryption.cs
+++ src/Dangl.Common/StringEncryption.cs
@@ -11,88 +11,112 @@ namespace Dangl
     /// </summary>
     public class StringEncryption
     {
-        private readonly Random _random;
-        private readonly byte[] _key;
-        private readonly Aes _aes = Aes.Create();
-        private readonly UTF8Encoding _encoder;
-        private const int VectorSize = 16;
+        private const int KEY_SIZE_BITS = 256;
+        private const int SALT_SIZE_BYTES = 32;
+        private const int PBKDF2_ITERATIONS = 1000;
 
-        /// <summary>
-        /// Basic constructor, requires the password.
-        /// </summary>
-        /// <param name="password">Must be between 16 and 32 characters. The characters must map to 16 to 32 bytes, so high unicode characters might count as up to 4.</param>
-        public StringEncryption(string password)
+        public static string EncryptString(string plainText, string password)
         {
-            if (password.Length > 32 || password.Length < 16)
+            if (string.IsNullOrWhiteSpace(plainText))
             {
-                throw new ArgumentOutOfRangeException(nameof(password), nameof(password) + " must be between 16 and 32 characters.");
+                throw new ArgumentNullException(nameof(plainText));
             }
-            var bytesFromPassword = Encoding.UTF8.GetBytes(password);
-            if (bytesFromPassword.Length > 32)
+            if (string.IsNullOrWhiteSpace(password))
             {
-                throw new ArgumentOutOfRangeException(nameof(password), nameof(password) + " length is valid, but UTF-8 bytes of password exceed allowed amount of 32 bytes. This may be due to high unicode characters needing as many as 4 bytes.");
-            }
-            bytesFromPassword = bytesFromPassword.Length == 32
-                ? bytesFromPassword
-                : bytesFromPassword.Concat(new byte[32 - bytesFromPassword.Length]).ToArray();
-            _random = new Random();
-            //System.Security.Cryptography.Aes.Create();
-            //RijndaelManagedInstance = new RijndaelManaged();
-            _encoder = new UTF8Encoding();
-            _key= bytesFromPassword;
-        }
-
-        /// <summary>
-        /// Returns the encrypted string.
-        /// </summary>
-        /// <param name="plainText">The string to encrypt.</param>
-        /// <returns></returns>
-        public string Encrypt(string plainText)
-        {
-            var vector = new byte[VectorSize];
-            _random.NextBytes(vector);
-            var cryptogram = vector.Concat(Encrypt(_encoder.GetBytes(plainText), vector));
-            return Convert.ToBase64String(cryptogram.ToArray());
-        }
-
-        /// <summary>
-        /// Returns a decrypted string.
-        /// </summary>
-        /// <param name="encryptedText">The encrypted string.</param>
-        /// <returns></returns>
-        public string Decrypt(string encryptedText)
-        {
-            var cryptogram = Convert.FromBase64String(encryptedText);
-            if (cryptogram.Length < VectorSize + 1)
-            {
-                throw new ArgumentException("Not a valid encrypted string", nameof(encryptedText));
+                throw new ArgumentNullException(nameof(password));
             }
 
-            var vector = cryptogram.Take(VectorSize).ToArray();
-            var buffer = cryptogram.Skip(VectorSize).ToArray();
-            return _encoder.GetString(Decrypt(buffer, vector));
-        }
-
-        private byte[] Encrypt(byte[] buffer, byte[] vector)
-        {
-            var encryptor = _aes.CreateEncryptor(_key, vector);
-            return Transform(buffer, encryptor);
-        }
-
-        private byte[] Decrypt(byte[] buffer, byte[] vector)
-        {
-            var decryptor = _aes.CreateDecryptor(_key, vector);
-            return Transform(buffer, decryptor);
-        }
-
-        private byte[] Transform(byte[] buffer, ICryptoTransform transform)
-        {
-            var stream = new MemoryStream();
-            using (var cryptoStream = new CryptoStream(stream, transform, CryptoStreamMode.Write))
+            var plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+            var saltBytes = new byte[SALT_SIZE_BYTES];
+            var randomNumberGenerator = RandomNumberGenerator.Create();
+            randomNumberGenerator.GetBytes(saltBytes);
+            var passwordBytes = new Rfc2898DeriveBytes(password, saltBytes, PBKDF2_ITERATIONS).GetBytes(KEY_SIZE_BITS / 8);
+            var aes = Aes.Create();
+            aes.Mode = CipherMode.CBC;
+            aes.Key = passwordBytes;
+            aes.GenerateIV();
+            var initVectorBytes = aes.IV;
+            var aesEncryptor = aes.CreateEncryptor();
+            using (var memoryStream = new MemoryStream())
             {
-                cryptoStream.Write(buffer, 0, buffer.Length);
+                using (var cryptoStream = new CryptoStream(memoryStream, aesEncryptor, CryptoStreamMode.Write))
+                {
+                    cryptoStream.Write(plainTextBytes, 0, plainTextBytes.Length);
+                    cryptoStream.FlushFinalBlock();
+                    var cipherTextBytes = memoryStream.ToArray();
+                    return $"{BitConverter.ToString(saltBytes).Replace("-", string.Empty)}:{BitConverter.ToString(initVectorBytes).Replace("-", string.Empty)}:{Convert.ToBase64String(cipherTextBytes)}";
+                }
             }
-            return stream.ToArray();
+        }
+
+        public static string DecryptString(string encryptedText, string password)
+        {
+            if (string.IsNullOrWhiteSpace(encryptedText))
+            {
+                throw new ArgumentNullException(nameof(encryptedText));
+            }
+            if (string.IsNullOrWhiteSpace(password))
+            {
+                throw new ArgumentNullException(nameof(password));
+            }
+            return DecryptString(encryptedText, password, false);
+        }
+
+        private static string DecryptString(string encryptedText, string password, bool useZeroPadding)
+        {
+            var splitText = encryptedText.Split(':');
+            if (splitText.Length != 3)
+            {
+                throw new FormatException("Expecting the decrypted text to be in the form of \"salt_hex:iv_hex:text_base64\"");
+            }
+            if (splitText[0].Length != SALT_SIZE_BYTES * 2)
+            {
+                throw new FormatException("Expecting the salt to be " + SALT_SIZE_BYTES + " bytes");
+            }
+            if (splitText[1].Length != KEY_SIZE_BITS / 8)
+            {
+                throw new FormatException("Expecting the IV to be " + (KEY_SIZE_BITS / (8 * 2)) + " bytes");
+            }
+
+            var encryptedTextBytes = Convert.FromBase64String(splitText[2]);
+            using (var memoryStream = new MemoryStream(encryptedTextBytes))
+            {
+                memoryStream.Position = 0;
+                var saltBytes = StringToByteArray(splitText[0]);
+                var initVectorBytes = StringToByteArray(splitText[1]);
+                var passwordBytes = new Rfc2898DeriveBytes(password, saltBytes, PBKDF2_ITERATIONS).GetBytes(KEY_SIZE_BITS / 8);
+                var aes = Aes.Create();
+                aes.Mode = CipherMode.CBC;
+                if (useZeroPadding)
+                {
+                    aes.Padding = PaddingMode.Zeros;
+                }
+                var aesDecryptor = aes.CreateDecryptor(passwordBytes, initVectorBytes);
+                try
+                {
+                    using (var cryptoStream = new CryptoStream(memoryStream, aesDecryptor, CryptoStreamMode.Read))
+                    {
+                        byte[] plainTextBytes = new byte[encryptedTextBytes.Length];
+
+                        int decryptedByteCount = cryptoStream.Read(plainTextBytes, 0, plainTextBytes.Length);
+                        return Encoding.UTF8.GetString(plainTextBytes, 0, decryptedByteCount);
+
+                    }
+                }
+                catch (CryptographicException)
+                {
+                    return DecryptString(encryptedText, password, true);
+                }
+            }
+        }
+
+
+        private static byte[] StringToByteArray(string hex)
+        {
+            return Enumerable.Range(0, hex.Length)
+                .Where(x => x % 2 == 0)
+                .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
+                .ToArray();
         }
     }
 }
--- test/Dangl.Common.Tests/StringEncryptionTests.cs
+++ test/Dangl.Common.Tests/StringEncryptionTests.cs
@@ -7,64 +7,82 @@ namespace Dangl.Common.Tests
     public class StringEncryptionTests
     {
         [Fact]
-        public void Fail_PasswordTooShort()
-        {
-            Assert.Throws(typeof (ArgumentOutOfRangeException), () => { var instance = new StringEncryption("0123456789ABCDE"); });
-        }
-
-        [Fact]
-        public void Fail_PasswordTooLong()
-        {
-            Assert.Throws(typeof (ArgumentOutOfRangeException), () => { var instance = new StringEncryption("0123456789ABCDEF0123456789ABCDEF_"); });
-        }
-
-        [Fact]
-        public void Fail_PasswordTooLongDueToHighASCIICharacters()
-        {
-            var passwordInput = "0123456789012345678901234567891\u26A1";
-            Assert.Equal(32, passwordInput.Length);
-            Assert.Throws(typeof(ArgumentOutOfRangeException), () => { var instance = new StringEncryption(passwordInput); });
-        }
-
-        [Fact]
-        public void Initialization_32CharacterPassword()
-        {
-            var passwordInput = "01234567890123456789012345678912";
-            var instance = new StringEncryption(passwordInput);
-        }
-
-        [Fact]
         public void EncryptDecrypt_RegularCharacters()
         {
             // Arrange
-            var instance = new StringEncryption("1234ffddddddddf5666");
-            var textToEncrypt = "Hello World!";
+            var password = "1234ffddddddddf5666";
+            var text = "Hello World!";
 
             // Act
-            var encryptedString01 = instance.Encrypt(textToEncrypt);
-            var encryptedString02 = instance.Encrypt(textToEncrypt);
-            var decryptedString01 = instance.Decrypt(encryptedString01);
-            var decryptedString02 = instance.Decrypt(encryptedString02);
+            var encryptedString01 = StringEncryption.EncryptString(text, password);
+            var encryptedString02 = StringEncryption.EncryptString(text, password);
+            var decryptedString01 = StringEncryption.DecryptString(encryptedString01, password);
+            var decryptedString02 = StringEncryption.DecryptString(encryptedString02, password);
 
             // Assert
-            Assert.Equal(textToEncrypt, decryptedString01); // Decrypted string should match original string
-            Assert.Equal(textToEncrypt, decryptedString02); // Decrypted string should match original string
-            Assert.NotEqual(textToEncrypt, encryptedString01); // Encrypted string should not match original string
+            Assert.Equal(text, decryptedString01); // Decrypted string should match original string
+            Assert.Equal(text, decryptedString02); // Decrypted string should match original string
+            Assert.NotEqual(text, encryptedString01); // Encrypted string should not match original string
             Assert.NotEqual(encryptedString01, encryptedString02); // String should never be encrypted the same twice
         }
+
+        [Fact]
+        public void EncryptTwiceWithSamePassword_DifferentRepresentation()
+        {
+            var password = "SomePassword";
+            var text = "Hello World!";
+            var encryptedString01 = StringEncryption.EncryptString(text, password);
+            var encryptedString02 = StringEncryption.EncryptString(text, password);
+            Assert.NotEqual(encryptedString01, encryptedString02);
+        }
+
+        [Fact]
+        public void CanNotDecryptWithPasswordThatStartsWithSameCharactersButIsDifferent()
+        {
+            var password_01 = "fahjdsfadsf987hdsafbd6fbasd90fvb6ads0fvb6das98fbsad0fvbads09fvbß9vbf6absdf6";
+            var password_02 = "fahjdsfadsf987hdsafbd6fbasd90fvb6ads0fvb6das98fbsad0fvbads09fvbß9vbf6absdf";
+            var text = "Hello World!";
+            var encryptedString = StringEncryption.EncryptString(text, password_01);
+            var decryptedString = StringEncryption.DecryptString(encryptedString, password_02);
+            Assert.NotEqual(text, decryptedString);
+        }
+
+        [Fact]
+        public void CanNotDecryptWithInvalidPassword()
+        {
+            var password = "some pass";
+            var text = "Hello World!";
+            var encryptedString = StringEncryption.EncryptString(text, password);
+            var invalidDecryptedString = StringEncryption.DecryptString(encryptedString, Guid.NewGuid().ToString());
+            Assert.NotEqual(text, invalidDecryptedString);
+        }
+
+        [Fact]
+        public void InvalidKeyDoesNotThrowExceptionDuringDeserialization()
+        {
+            var password = "some pass";
+            var text = "Hello World!";
+            var encryptedString = StringEncryption.EncryptString(text, password);
+            for (int i = 0; i < 50; i++)
+            {
+                var invalidDecryptedString = StringEncryption.DecryptString(encryptedString, Guid.NewGuid().ToString());
+                Assert.NotEqual(text, invalidDecryptedString);
+            }
+        }
+
 
         [Fact]
         public void EncryptDecrypt_HighUnicodeCharacters()
         {
             // Arrange
-            var instance = new StringEncryption("1234ffdddddddd\u26A1f5666");
+            var password = "1234ffdddddddd\u26A1f5666";
             var textToEncrypt = "Hello\u26A1\u26A1\u26A1\u26A1\u26A1 World!";
 
             // Act
-            var encryptedString01 = instance.Encrypt(textToEncrypt);
-            var encryptedString02 = instance.Encrypt(textToEncrypt);
-            var decryptedString01 = instance.Decrypt(encryptedString01);
-            var decryptedString02 = instance.Decrypt(encryptedString02);
+            var encryptedString01 = StringEncryption.EncryptString(textToEncrypt, password);
+            var encryptedString02 = StringEncryption.EncryptString(textToEncrypt, password);
+            var decryptedString01 = StringEncryption.DecryptString(encryptedString01, password);
+            var decryptedString02 = StringEncryption.DecryptString(encryptedString02, password);
 
             // Assert
             Assert.Equal(textToEncrypt, decryptedString01); // Decrypted string should match original string
@@ -74,18 +92,155 @@ namespace Dangl.Common.Tests
         }
 
         [Fact]
-        public void EncryptDecrypt_HighUnicodeCharacters_ThrowsException()
+        public void ArgumentNullExceptionOnNullText_Encrypt()
         {
-            // Arrange
-            Assert.Throws(typeof (ArgumentOutOfRangeException), () => new StringEncryption("\u26A1\u26A1\u26A1\u26A1\u26A1\u26A1\u26A1\u26A1\u26A1"));
+            string inputText = null;
+            string password = "Hello Password!";
+            Assert.Throws(typeof(ArgumentNullException), () => StringEncryption.EncryptString(inputText, password));
         }
 
         [Fact]
-        public void Decrypt_ValidBase64StringButTooShort()
+        public void ArgumentNullExceptionOnEmptyText_Encrypt()
         {
-            var input = Convert.ToBase64String(Encoding.UTF8.GetBytes("123456"));
-            var encryptionInstance = new StringEncryption("1234567890123456");
-            Assert.Throws(typeof (ArgumentException), () => encryptionInstance.Decrypt(input));
+            string inputText = string.Empty;
+            string password = "Hello Password!";
+            Assert.Throws(typeof(ArgumentNullException), () => StringEncryption.EncryptString(inputText, password));
+        }
+
+        [Fact]
+        public void ArgumentNullExceptionOnNullPassword_Encrypt()
+        {
+            string inputText = "Hello World";
+            string password = null;
+            Assert.Throws(typeof(ArgumentNullException), () => StringEncryption.EncryptString(inputText, password));
+        }
+
+        [Fact]
+        public void ArgumentNullExceptionOnEmptyPassword_Encrypt()
+        {
+            string inputText = "Hello World";
+            string password = string.Empty;
+            Assert.Throws(typeof(ArgumentNullException), () => StringEncryption.EncryptString(inputText, password));
+        }
+
+        [Fact]
+        public void ArgumentNullExceptionOnNullTextAndPassword_Encrypt()
+        {
+            string inputText = null;
+            string password = null;
+            Assert.Throws(typeof(ArgumentNullException), () => StringEncryption.EncryptString(inputText, password));
+        }
+
+        [Fact]
+        public void ArgumentNullExceptionOnEmptyTextAndPassword_Encrypt()
+        {
+            string inputText = string.Empty;
+            string password = string.Empty;
+            Assert.Throws(typeof(ArgumentNullException), () => StringEncryption.EncryptString(inputText, password));
+        }
+
+        [Fact]
+        public void ArgumentNullExceptionOnNullText_Decrypt()
+        {
+            string inputText = null;
+            string password = "Hello Password!";
+            Assert.Throws(typeof(ArgumentNullException), () => StringEncryption.DecryptString(inputText, password));
+        }
+
+        [Fact]
+        public void ArgumentNullExceptionOnEmptyText_Decrypt()
+        {
+            string inputText = string.Empty;
+            string password = "Hello Password!";
+            Assert.Throws(typeof(ArgumentNullException), () => StringEncryption.DecryptString(inputText, password));
+        }
+
+        [Fact]
+        public void ArgumentNullExceptionOnNullPassword_Decrypt()
+        {
+            string inputText = "Hello World!";
+            string password = null;
+            Assert.Throws(typeof(ArgumentNullException), () => StringEncryption.DecryptString(inputText, password));
+        }
+
+        [Fact]
+        public void ArgumentNullExceptionOnEmptyPassword_Decrypt()
+        {
+            string inputText = "Hello World!";
+            string password = string.Empty;
+            Assert.Throws(typeof(ArgumentNullException), () => StringEncryption.DecryptString(inputText, password));
+        }
+
+        [Fact]
+        public void ArgumentNullExceptionOnNullTextAndPassword_Decrypt()
+        {
+            string inputText = null;
+            string password = null;
+            Assert.Throws(typeof(ArgumentNullException), () => StringEncryption.DecryptString(inputText, password));
+        }
+
+        [Fact]
+        public void ArgumentNullExceptionOnEmptyTextAndPassword_Decrypt()
+        {
+            string inputText = string.Empty;
+            string password = string.Empty;
+            Assert.Throws(typeof(ArgumentNullException), () => StringEncryption.DecryptString(inputText, password));
+        }
+
+        [Fact]
+        public void FormatException_IVTooShort()
+        {
+            var fabricatedDecryptedText = "2DEC9C6991943FCC4CEDE77173AF229E149ACF1877F1F2DD0BE332CD43429418:0DD41EE10F3FE2EA9328C898F03F231:H8LR+VN7diSBZeylsYVVGw==";
+            var password = "Hello Password!";
+            Assert.Throws(typeof(FormatException), () => StringEncryption.DecryptString(fabricatedDecryptedText, password));
+        }
+
+        [Fact]
+        public void FormatException_IVTooLong()
+        {
+            var fabricatedDecryptedText = "2DEC9C6991943FCC4CEDE77173AF229E149ACF1877F1F2DD0BE332CD43429418:760DD41EE10F3FE2EA9328C898F03F231:H8LR+VN7diSBZeylsYVVGw==";
+            var password = "Hello Password!";
+            Assert.Throws(typeof(FormatException), () => StringEncryption.DecryptString(fabricatedDecryptedText, password));
+        }
+
+        [Fact]
+        public void FormatException_SaltTooShort()
+        {
+            var fabricatedDecryptedText = "DEC9C6991943FCC4CEDE77173AF229E149ACF1877F1F2DD0BE332CD43429418:60DD41EE10F3FE2EA9328C898F03F231:H8LR+VN7diSBZeylsYVVGw==";
+            var password = "Hello Password!";
+            Assert.Throws(typeof(FormatException), () => StringEncryption.DecryptString(fabricatedDecryptedText, password));
+        }
+
+        [Fact]
+        public void FormatException_SaltTooLong()
+        {
+            var fabricatedDecryptedText = "22DEC9C6991943FCC4CEDE77173AF229E149ACF1877F1F2DD0BE332CD43429418:60DD41EE10F3FE2EA9328C898F03F231:H8LR+VN7diSBZeylsYVVGw==";
+            var password = "Hello Password!";
+            Assert.Throws(typeof(FormatException), () => StringEncryption.DecryptString(fabricatedDecryptedText, password));
+        }
+
+        [Fact]
+        public void FormatException_EncryptedPartNotBase64()
+        {
+            var fabricatedDecryptedText = "2DEC9C6991943FCC4CEDE77173AF229E149ACF1877F1F2DD0BE332CD43429418:60DD41EE10F3FE2EA93ü8C898F03F231:H8LR+VN7diSBZeylsYVVGw==";
+            var password = "Hello Password!";
+            Assert.Throws(typeof(FormatException), () => StringEncryption.DecryptString(fabricatedDecryptedText, password));
+        }
+
+        [Fact]
+        public void FormatException_TooFewSegments()
+        {
+            var fabricatedDecryptedText = "2DEC9C6991943FCC4CEDE77173AF229E149ACF1877F1F2DD0BE332CD43429418:H8LR+VN7diSBZeylsYVVGw==";
+            var password = "Hello Password!";
+            Assert.Throws(typeof(FormatException), () => StringEncryption.DecryptString(fabricatedDecryptedText, password));
+        }
+
+        [Fact]
+        public void FormatException_TooManySegments()
+        {
+            var fabricatedDecryptedText = "2DEC9C6991943FCC4CEDE77173AF229E149ACF1877F1F2DD0BE332CD43429418:60DD41EE10F3FE2EA9328C898F03F231:43:H8LR+VN7diSBZeylsYVVGw==";
+            var password = "Hello Password!";
+            Assert.Throws(typeof(FormatException), () => StringEncryption.DecryptString(fabricatedDecryptedText, password));
         }
     }
 }
