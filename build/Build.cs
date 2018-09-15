00e2495822f03b22f27f575ef12a512fc56eb126
Georg Dangl <georg@dangl.me>
2018-09-15 14:56:55 +0200

    Exclude NuGet packages from generated docs

--- build/Build.cs
+++ build/Build.cs
@@ -248,7 +248,7 @@ class Build : NukeBuild
             WebDocu(s => s
                 .SetDocuApiEndpoint(DocuApiEndpoint)
                 .SetDocuApiKey(DocuApiKey)
-                .SetSourceDirectory(OutputDirectory)
+                .SetSourceDirectory(OutputDirectory / "docs")
                 .SetVersion(GitVersion.NuGetVersion)
             );
         });
